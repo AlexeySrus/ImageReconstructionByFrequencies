64212ca95722b0fe455218153faaa95f568b9946
Alexey <alexey.s.russ@mail.ru>
2023-10-29 21:04:38 +0300

    Attempt to add adv loss

--- research_pipelines/denoising_with_wavelets/pytorch_wavelet_train.py
+++ research_pipelines/denoising_with_wavelets/pytorch_wavelet_train.py
@@ -217,7 +217,7 @@ class CustomTrainingPipeline(object):
         
 
         self.images_criterion = MIXLoss() # torch.nn.MSELoss()
-        # self.perceptual_loss = DISTS()
+        self.perceptual_loss = DISTS()
         # self.perceptual_loss = None
         # self.final_hist_loss = HistLoss(image_size=128, device=self.device)
         self.final_hist_loss = None
